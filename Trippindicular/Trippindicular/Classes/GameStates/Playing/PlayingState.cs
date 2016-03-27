50e407a88650e4b104c9a1f0ef9ae595f15e46b9
Timo van Milligen <cobravspanda01@gmail.com>
2016-03-27 16:42:31 +0200

    adding the playingstate I discarded

--- Trippindicular/Trippindicular/Classes/GameStates/Playing/PlayingState.cs
+++ Trippindicular/Trippindicular/Classes/GameStates/Playing/PlayingState.cs
@@ -2,6 +2,8 @@
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 using System;
+using System.Collections.Generic;
+
 
 //Class used to update and draw everything that is needed when the player is playing the game.
 class PlayingState : IGameLoopObject
@@ -10,7 +12,6 @@ class PlayingState : IGameLoopObject
 
     public PlayingState()
     {
-  
     }
     public void Initialize()
     {
@@ -29,6 +30,11 @@ class PlayingState : IGameLoopObject
         GameData.LevelObjects.HandleInput(ih);
         if (ih.KeyPressed(Keys.F5))
             GameSettings.SetFullscreen(!GameSettings.Fullscreen);
+        List<string> ls = GameData.LevelObjects.getActionOutputs();
+        foreach (string s in ls)
+        {
+            Console.WriteLine(s);
+        }
     }
 
     public void Reset()
